54df2c99938708ca8e7e30629dd54cb14f001b14
Caleb Choi <ccaleb7@vt.edu>
2020-05-02 22:02:51 -0400

    Game now works for winning on a row

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 #Holds and keeps track of the board positions
 board = ["-", "-","-",
-		 "-", "-","-",
-		 "-", "-","-",]
+         "-", "-","-",
+         "-", "-","-",]
 
 #Keep track of the current player between X and O, X goes first
 current_player = "X"
@@ -15,81 +15,110 @@ win = None
 continuing = True
 
 def display_board():
-	print("\n")
-	print(board[0] + " | " + board[1] + " | " + board[2])
-	print(board[3] + " | " + board[4] + " | " + board[5])
-	print(board[6] + " | " + board[7] + " | " + board[8])
-	print("\n")
+    print("\n")
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+    print("\n")
 
 def play():
-	display_board()
-	
-	while continuing:
-		make_move(current_player)
-		
-		check_win()
-		
-		check_tie()
-		
-		flip_player()
-	
-	if win == "X":
-		print("Player X has won!")
-	elif win == "O":
-		print("Player O has won!")
-	else:
-		print("Tie Game!")
+    #Display the board
+    display_board()
+    
+    #While the game can continue (no one has won or tied)
+    while continuing:
+        #Player makes their move
+        make_move(current_player)
+        
+        #Check to see if a player has won after that turn
+        check_win()
+        
+        #If no player has won, check to see if there is a tie
+        check_tie()
+        
+        #If no one has won or tied, flip the current player
+        flip_player()
+    
+    #After leaving the while loop, there should be a winner or a tie game
+    if win == "X":
+        print("Player X has won!")
+    elif win == "O":
+        print("Player O has won!")
+    else:
+        print("Tie Game!")
 
 def make_move(player):
-	print(player + "'s turn")
-	#Ask the player to input a cell to choose from
-	pos = input("Pick a position from 0 - 8: ")
-	
-	works = False
-	while not works:
-		#Make sure that the input is valid
-		while pos not in ["0", "1", "2", "3", "4", "5", "6", "7", "8"]:
-			pos = input("Please pick a position from 0 - 8: ")
-		
-		pos = int(pos)
-		
-		#Check to make sure that the spot is not occupied
-		if board[pos] == "-":
-			works = True
-		else:
-			print("That spot is occupied. Try again.")
-	
-	#Mark the position with the player's character
-	board[pos] = player
-	
-	#Display the board with the newly marked cell
-	display_board()
+    print(player + "'s turn")
+    #Ask the player to input a cell to choose from
+    pos = input("Pick a position from 0 - 8: ")
+    
+    works = False
+    while not works:
+        #Make sure that the input is valid
+        while pos not in ["0", "1", "2", "3", "4", "5", "6", "7", "8"]:
+            pos = input("Please pick a position from 0 - 8: ")
+        
+        pos = int(pos)
+        
+        #Check to make sure that the spot is not occupied
+        if board[pos] == "-":
+            works = True
+        else:
+            print("That spot is occupied. Try again.")
+    
+    #Mark the position with the player's character
+    board[pos] = player
+    
+    #Display the board with the newly marked cell
+    display_board()
 
-	
+    
 def flip_player():
-	#We need to use the global variable current_player
-	global current_player
-	
-	if current_player == "X":
-		current_player = "O"
-	else:
-		current_player = "X"
-		
-	
+    #We need to use the global variable current_player
+    global current_player
+    
+    if current_player == "X":
+        current_player = "O"
+    else:
+        current_player = "X"
+        
+    
 def check_win():
-	global continuing
+    global continuing
+    global win
+    
+    #Check if there is a winner in rows
+    row1 = board[0] == board[1] == board[2] != "-"
+    row2 = board[3] == board[4] == board[5] != "-"
+    row3 = board[6] == board[7] == board[8] != "-"
+    
+    if row1:
+            continuing = False
+            win = board[0]
+    elif row2:
+            continuing = False
+            win = board[3]
+    elif row3:
+            continuing = False
+            win = board[6]
+    else:
+        win = None
+    
+    #Check if there is a winner in columns
+    
+    #Check if there is a winner in diagonal
 
 
 def check_tie():
-	#Make use of global variable continuing
-	global continuing
-	
-	#If there are no more "-" spaces, and neither player has won, the game is tied
-	if "-" not in board:
-		continuing = False
-		return True
-	else:
-		return False
-	
+    #Make use of global variable continuing
+    global continuing
+    
+    #If there are no more "-" spaces, and neither player has won, the game is tied
+    if "-" not in board:
+        continuing = False
+        return True
+    else:
+        return False
+    
 
 play()
